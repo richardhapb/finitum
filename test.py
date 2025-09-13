a02fca1ff8d7fb33a7998625ae9887cce174794d
richardhapb <richard.hapb@icloud.com>
2025-09-12 21:34:24 -0300

    feat: add test

---
+++ test.py
@@ -0,0 +1,35 @@
+text = b'{\n    "subject": "Cargo en Cuenta",\n     "content": "
+[http://contentz.mkt8988.com/lp/14944/107881/BCH_nuevo.png] Richard Hector
+Alexander Pe a Bonifaz:Te informamos que se ha realizado una compra por $2.872
+con cargo a Cuenta ****7204 en GRAN AVENIDA el 12/09/2025 15:20.\nRevisa Saldos
+y Movimientos en App Mi Banco o Banco en L\xc3\xadnea.\n\n\nM\xc3\xa1s
+informaci\xc3\xb3n 60\xe2\x80\x8d0 63\xe2\x80\x8d7
+37\xe2\x80\x8d37.\n\n[http://contentz.mkt8988.com/lp/14944/107981/aleta_BCH.png]\nSigue
+estos consejos para evitar
+fraudes\n\n[http://contentz.mkt8988.com/lp/14944/107981/ticket_BCH.png] Nunca
+te llamaremos solicitando tus claves o informaci\xc3\xb3n
+personal.\n\n[http://contentz.mkt8988.com/lp/14944/107981/ticket_BCH.png] Nunca
+hagas click en links ni descargues archivos adjuntos de correos
+sospechosos.\n\n[http://contentz.mkt8988.com/lp/14944/107981/ticket_BCH.png]
+Ingresa a la p\xc3\xa1gina del Banco digitando la direcci\xc3\xb3n en la barra
+de tu navegador.\n\nRealiza todo de forma \xc3\xa1gil y simple usando nuestras
+aplicaciones*\n\n[http://contentz.mkt8988.com/lp/14944/107981/mi_banco.png]\n\nMi
+Banco\n\n[http://contentz.mkt8988.com/lp/14944/107881/MIPASSnuevo.png]\n\n\xe2\x80\x8dMi
+Pass\n\n[http://contentz.mkt8988.com/lp/14944/107981/mi_inversion.png]\n\nMi
+Inversi\xc3\xb3n\n\n\n\nEncu\xc3\xa9ntranos\n\n[http://contentz.mkt8988.com/lp/14944/107981/facebook.png]
+bancochile\xe2\x80\x8e.\xe2\x80\x8dcl\n\n[http://contentz.mkt8988.com/lp/14944/107881/twe-X-BCH.png]
+@\xe2\x80\x8dAyudaBancoChile\n\n[http://contentz.mkt8988.com/lp/14944/107981/instagram.png]
+@\xe2\x80\x8dBancodechile\n\n[http://contentz.mkt8988.com/lp/14944/107981/telefono_2.png]
+Banca Telef\xc3\xb3nica\n\n60\xe2\x80\x8d0 63\xe2\x80\x8d7
+37\xe2\x80\x8d37\n\n* Por seguridad, descarga las aplicaciones solo en las
+tiendas Google Play y App Store. Las Apps no est\xc3\xa1n disponibles para
+tel\xc3\xa9fonos desbloqueados (Jailbreak o Rooteado).\n\nEste mensaje ha sido
+enviado a \'richard.penab@gmail.com\' con informaci\xc3\xb3n exclusiva para
+clientes del banco. Banco de Chile. Casa Matriz: Ahumada 251, Santiago de
+Chile.\nInf\xc3\xb3rmese sobre la garant\xc3\xada estatal de los
+dep\xc3\xb3sitos en su banco o en
+www\xe2\x80\x8e.\xe2\x80\x8dcmfchile\xe2\x80\x8e.\xe2\x80\x8dcl
+\xc2\xa9.\nTodos los derechos reservados.\nHoja
+[http://contentz.mkt8988.com/lp/14944/107981/hoja.png] Comprometidos por un
+medio ambiente mejor, prefiera los medios digitales al papel impreso.
+[http://bancochile.cl/img/PS76JRn/pIolTcsauGpvWr527XL/belEcd0GnH+ooDQ=ttp://bancochile.cl/img/${id}]"\n}'
